15161b7e22569d17921b8b5d5059da223e764dea
dennisgsmith <dennisgsmith12@gmail.com>
2020-12-02 08:41:33 -0500

    Add python scripts to get submissions and process data

---
+++ get_submissions.py
@@ -0,0 +1,81 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
+from dotenv import load_dotenv
+from pathlib import Path
+import time
+import os
+
+
+def site_login(browser, login_url):
+    '''
+    takes in driver and logs into account
+    '''
+    browser.get(login_url)
+    browser.find_element_by_id("user_account_email").send_keys(USER)
+    browser.find_element_by_id("user_account_password").send_keys(PASS)
+    browser.find_element_by_name("commit").click()
+
+
+def submissions_download(browser, submissions_url):
+    '''
+    navigates to submissions page and initates proper download
+    '''
+    browser.get(submissions_url) # go to submissions tab url
+    browser.find_element_by_xpath('//*[@id="submission-index"]/div[3]/div[5]/div[2]/a[2]').click() # open dropdown
+    browser.find_element_by_xpath('//*[@id="dropdown-export-content"]/ul/li[1]/a').click() # select element from dropdown
+    WebDriverWait(browser, 1) # wait 1 second for javascript motion-window
+    browser.find_element_by_id('export_configuration').click() # export config dropdown
+    browser.find_element_by_xpath('//*[@id="export_configuration"]/option[2]').click() # select "Basic Summary"
+    browser.find_element_by_xpath('/html/body/div[8]/div[2]/div/form/div[3]/button').click() # click export button
+
+
+def download_handler(browser, download_dir):
+    '''
+    Take in driver and downlad file to specified directory
+    '''
+    browser.command_executor._commands["send_command"] = ("POST", '/session/$sessionId/chromium/send_command')
+    params = {'cmd':'Page.setDownloadBehavior', 'params': {'behavior': 'allow', 'downloadPath': download_dir}}
+    browser.execute("send_command", params)
+
+
+HERE = Path(__file__).parent
+DATA_FOLDER = HERE / 'data'
+
+load_dotenv('.env')
+USER = os.getenv('USER')
+PASS = os.getenv('PASS')
+
+### BOILERPLATE settings browser ###
+chrome_options = Options()
+# chrome_options.add_argument("--headless") # comment out this line if you want to see the action
+chrome_options.add_argument("--window-size=1920x1080")
+chrome_options.add_argument("--disable-notifications")
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--verbose')
+chrome_options.add_experimental_option("prefs", {
+        "download.default_directory": f"{DATA_FOLDER}",
+        "download.prompt_for_download": False,
+        "download.directory_upgrade": True,
+        "safebrowsing_for_trusted_sources_enabled": False,
+        "safebrowsing.enabled": False
+})
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--disable-software-rasterizer')
+
+# initialize chromedriver and ingest its system path
+browser =  webdriver.Chrome(options=chrome_options) # make sure chromedriver is in system PATH or specify "executable_path" kwarg
+login_url = "https://filmfreeway.com/login"
+submissions_url = "https://filmfreeway.com/submissions"
+dl_path = str(DATA_FOLDER)
+
+# run the program
+if __name__=="__main__":
+    try:
+        site_login(browser, login_url)
+        submissions_download(browser, submissions_url)
+        download_handler(browser, dl_path)
+        time.sleep(100) # long enough to export all submissions
+        browser.quit()
+    except Exception as exc:
+        print(f"There was an error: {exc}")
---
+++ process_data.py
@@ -0,0 +1,60 @@
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from collections import Counter
+from pathlib import Path
+
+### ETL ###
+HERE = Path(__file__).parent
+DATA_FOLDER = HERE / "data"
+
+raw = pd.read_csv(DATA_FOLDER / "filmfreeway-submissions.csv")
+subs_df = raw.copy() # dont step on the raw data, assumes data is local
+
+# Drop withdrawn entries
+subs_df = subs_df[subs_df['Submission Status'] != 'Withdrawn']
+duplicate_entries = subs_df[subs_df['Project Title'].duplicated()==True]
+# print(f"List of Duplicate Entries: {list(duplicate_entries['Project Title'])}")
+
+# Replace column to shorten: Feel Good Shorts
+subs_df['Submission Categories'] = subs_df['Submission Categories'].apply( \
+        lambda x: x.replace('Short films that make you feel good.', 'Feel Good Shorts'))
+
+# Correct 'Youth Short Films, Short Documentary' column
+print(subs_df.info())
+# Force to 'Short Documentary' for now
+subs_df['Submission Categories'] = subs_df['Submission Categories'].apply( \
+        lambda x: x.replace('Youth Short Films, Short Documentary', 'Short Documentary'))
+# TODO: until further investigation
+
+
+subs_df.head()
+
+subs_per_cat = dict(Counter(subs_df['Submission Categories']))
+sorted_sub_cat = sorted(subs_per_cat.items(), key=lambda x: x[1])
+total_subs = len(subs_df['Project Title'])
+
+
+# PLOTTING
+labels = [k for k, v in sorted_sub_cat]
+ratios = [(v / total_subs) for k, v in sorted_sub_cat]
+n_subs = [v for k, v in sorted_sub_cat]
+y_pos = np.arange(len(labels)) # Label locations
+
+# BAR CHART
+plt.rcdefaults()
+fig, ax = plt.subplots()
+_ = ax.barh(y_pos, n_subs, align='center')
+_ = ax.set_yticks(y_pos)
+_ = ax.set_yticklabels(labels)
+_ = ax.invert_yaxis()
+_ = ax.set_xlabel('Number of Submissions')
+_ = ax.set_title('Entries per Submission Category')
+plt.show()
+
+# PIE CHART
+fig1, ax1 = plt.subplots()
+_ = ax1.pie(ratios, labels=labels, autopct='%1.1f%%', shadow=True)
+_ = ax1.axis('equal') # make sure pie is circle
+plt.show()
