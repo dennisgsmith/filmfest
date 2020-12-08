11a79386b47b593ba6bca1ebc933ab7788d7c013
dennisgsmith <dennisgsmith12@gmail.com>
2020-12-08 13:50:06 -0500

    Add to .gitignore, rename process_data.py to process.py, generate .viz .png from process

--- process.py
+++ process.py
@@ -9,13 +9,13 @@ from pathlib import Path
 HERE = Path(__file__).parent
 DATA_FOLDER = HERE / "data"
 
+# read in csv to pd.DataFrame
 raw = pd.read_csv(DATA_FOLDER / "filmfreeway-submissions.csv")
 subs_df = raw.copy() # dont step on the raw data, assumes data is local
 
 # Drop withdrawn entries
 subs_df = subs_df[subs_df['Submission Status'] != 'Withdrawn']
 duplicate_entries = subs_df[subs_df['Project Title'].duplicated()==True]
-# print(f"List of Duplicate Entries: {list(duplicate_entries['Project Title'])}")
 
 # Replace column to shorten: Feel Good Shorts
 subs_df['Submission Categories'] = subs_df['Submission Categories'].apply( \
@@ -26,10 +26,6 @@ print(subs_df.info())
 # Force to 'Short Documentary' for now
 subs_df['Submission Categories'] = subs_df['Submission Categories'].apply( \
         lambda x: x.replace('Youth Short Films, Short Documentary', 'Short Documentary'))
-# TODO: until further investigation
-
-
-subs_df.head()
 
 subs_per_cat = dict(Counter(subs_df['Submission Categories']))
 sorted_sub_cat = sorted(subs_per_cat.items(), key=lambda x: x[1])
@@ -51,10 +47,12 @@ _ = ax.set_yticklabels(labels)
 _ = ax.invert_yaxis()
 _ = ax.set_xlabel('Number of Submissions')
 _ = ax.set_title('Entries per Submission Category')
-plt.show()
+_ = plt.tight_layout()
+plt.savefig(DATA_FOLDER / 'EntriesPerCat_BAR.png')
 
 # PIE CHART
 fig1, ax1 = plt.subplots()
-_ = ax1.pie(ratios, labels=labels, autopct='%1.1f%%', shadow=True)
+_ = ax1.pie(ratios, labels=labels, autopct='%1.1f%%', shadow=True, normalize=True)
+_ = ax.set_title('Entries per Submission Category')
 _ = ax1.axis('equal') # make sure pie is circle
-plt.show()
+plt.savefig(DATA_FOLDER / 'EntriesPerCat_PIE.png', bbox_inches='tight')
