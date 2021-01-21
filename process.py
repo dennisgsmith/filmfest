3904f98f0048442c0239feb2d593baebf5991c1a
dennisgsmith <dennisgsmith12@gmail.com>
2021-01-21 09:24:18 -0500

    ReformattCcleaning script and update input data

--- process.py
+++ process.py
@@ -4,6 +4,7 @@ import pandas as pd
 import numpy as np
 from collections import Counter
 from pathlib import Path
+import math
 
 
 HERE = Path(__file__).parent
@@ -11,57 +12,73 @@ DATA_FOLDER = HERE / "data"
 
 # read in csv to pd.DataFrame
 subs_df = pd.read_csv(DATA_FOLDER / "filmfreeway-submissions.csv")
-desc_df = pd.read_csv(DATA_FOLDER / "descriptions.csv")
+
 ### CLEAN ###
 
 # Drop withdrawn entries
-subs_df = subs_df[subs_df['Submission Status'] != 'Withdrawn']
+subs_df = subs_df[subs_df["Submission Status"] != "Withdrawn"]
 
 # Replace column to shorten: Feel Good Shorts
-subs_df['Submission Categories'] = subs_df['Submission Categories'].apply(
-        lambda x: x.replace('Short films that make you feel good.', 'Feel Good Shorts')
+subs_df["Submission Categories"] = subs_df["Submission Categories"].apply(
+    lambda x: x.replace("Short films that make you feel good.", "Feel Good Shorts")
 )
 
 # 'Youth Short Films, Short Documentary' force to 'Short Documentary'
-subs_df['Submission Categories'] = subs_df['Submission Categories'].apply(
-        lambda x: x.replace('Youth Short Films, Short Documentary', 'Short Documentary')
+subs_df["Submission Categories"] = subs_df["Submission Categories"].apply(
+    lambda x: x.replace("Youth Short Films, Short Documentary", "Short Documentary")
 )
 
 # drop rating, genres cols due to all missing data
-subs_df.drop(columns=['Rating', 'Genres'], inplace=True)
+subs_df.drop(columns=["Rating", "Genres"], inplace=True)
 
 # fill na with empty str to allow apply method
-subs_df['State'].fillna('', inplace=True)
+subs_df["State"].fillna("", inplace=True)
 
 # replace to unify state names
-subs_df['State'] = subs_df['State'].apply(
-    lambda x: x.replace('maryland', 'MD')
-               .replace('Maryland', 'MD')
-               .replace('Virginia', 'VA')
-               .replace('Georgia', 'GA')
-               .replace('Pennsylvania', 'PA')
-               .replace('Florida', 'FL')
-               .replace('New York', 'NY')
-               .replace('ny', 'NY')
+subs_df["State"] = subs_df["State"].apply(
+    lambda x: x.replace("maryland", "MD")
+    .replace("Maryland", "MD")
+    .replace("Virginia", "VA")
+    .replace("Georgia", "GA")
+    .replace("Pennsylvania", "PA")
+    .replace("Florida", "FL")
+    .replace("New York", "NY")
+    .replace("ny", "NY")
 )
 
 # SUBSET SELECTED FILMS
-selected_df = subs_df[subs_df['Judging Status']=='Selected']
+selected_df = subs_df[subs_df["Judging Status"] == "Selected"]
 
-pd.to_timedelta(selected_df['Duration'])
+# Convert Duration column to timedelta datatype
+pd.to_timedelta(selected_df.Duration)
+
+selected_df["Duration"] = round((selected_df["Duration"] / np.timedelta64(1, "m")))
+
+# subset of total time and number of films per category
+selected_duration = selected_df.loc[:, ("Submission Categories", "Duration")]
 
 # new dataframe without necceary columns for export
-selected_excel_export = selected_df.drop(columns=['Submission Status', 'Submission ID', 'Birthdate', 'Gender', 'Judging Status', 'Submission Date'])
+selected_excel_export = selected_df.drop(
+    columns=[
+        "Submission Status",
+        "Submission ID",
+        "Birthdate",
+        "Gender",
+        "Judging Status",
+        "Submission Date",
+    ]
+)
 
 # Output new spreasheet for Excel
-selected_excel_export.sort_values(by='Submission Categories').to_excel(DATA_FOLDER / 'OCFF-2021_selected-films_01-12-21.xlsx')
-desc_df.to_excel(DATA_FOLDER / 'descriptions.xlsx')
+selected_excel_export.sort_values(by="Submission Categories").to_excel(
+    DATA_FOLDER / "OCFF-2021_selected-films_01-14-21.xlsx"
+)
 
 # DEFINE PLOTTING FUNCTIONS
 
-subs_per_cat = dict(Counter(selected_df['Submission Categories']))
+subs_per_cat = dict(Counter(selected_df["Submission Categories"]))
 sorted_cat = sorted(subs_per_cat.items(), key=lambda x: x[1])
-total_selected = len(selected_df['Project Title'])
+total_selected = len(selected_df["Project Title"])
 labels = [k for k, v in sorted_cat]
 number_of_entries = [v for k, v in sorted_cat]
 ratios = [(v / total_selected) for k, v in sorted_cat]
@@ -70,18 +87,18 @@ ratios = [(v / total_selected) for k, v in sorted_cat]
 # bar
 plt.rcdefaults()
 fig, ax = plt.subplots()
-_ = ax.barh(np.arange(len(labels)), number_of_entries, align='center')
+_ = ax.barh(np.arange(len(labels)), number_of_entries, align="center")
 _ = ax.set_yticks(np.arange(len(labels)))
 _ = ax.set_yticklabels(labels)
 _ = ax.invert_yaxis()
-_ = ax.set_xlabel('Number of Sumbissions')
-_ = ax.set_title('Submissions Per Category')
+_ = ax.set_xlabel("Number of Sumbissions")
+_ = ax.set_title("Submissions Per Category")
 _ = plt.tight_layout()
-plt.savefig(DATA_FOLDER / 'entries-per-cat-bar.png')
+plt.savefig(DATA_FOLDER / "entries-per-cat-bar.png")
 
 # pie
 fig1, ax = plt.subplots()
-_ = ax.pie(ratios, labels=labels, autopct='%1.1f%%', shadow=False, normalize=True)
-_ = ax.set_title('Submissions Per Category')
-_ = ax.axis('equal') # make sure pie is circle
-plt.savefig(DATA_FOLDER / 'entries-per-cat-pie.png', bbox_inches='tight')
+_ = ax.pie(ratios, labels=labels, autopct="%1.1f%%", shadow=False, normalize=True)
+_ = ax.set_title("Submissions Per Category")
+_ = ax.axis("equal")  # make sure pie is circle
+plt.savefig(DATA_FOLDER / "entries-per-cat-pie.png", bbox_inches="tight")
--- settings.py
+++ settings.py
@@ -2,21 +2,22 @@ from dotenv import load_dotenv
 from pathlib import Path
 import os
 
+
 def get_user_creds():
-    '''
+    """
     check env for previously stored creds
     if none create local env to store user input creds
     return USER and PASS
-    '''
-    env_path = Path('.') / '.env'
-    if Path.is_file(env_path): # you already logged in successfully
+    """
+    env_path = Path(".") / ".env"
+    if Path.is_file(env_path):  # you already logged in successfully
         pass
-    else: # you have not yet logged in successfully
+    else:  # you have not yet logged in successfully
         with open(".env", "w") as f:
             f.write("USER = '{}'".format(str(input("Enter username: "))))
             f.write("\n")
             f.write("PASS = '{}'".format(str(input("Enter password: "))))
-        
+
     load_dotenv(dotenv_path=env_path)
     USER = os.getenv("USER")
     PASS = os.getenv("PASS")
